8bc2ba95d0232dfd277a5d6b4378b08db719cc71
MAMAMAisused <vin_mlc@126.com>
2019-02-17 23:29:18 +0800

    初始框架。可以各个方向移动。

--- PacMan.py
+++ PacMan.py
@@ -4,8 +4,17 @@ from pygame.locals import *
 import MaColor
 import os.path
 import numpy
+import threading
+import time
 
 global SCREEN
+global CLOCKTICKFLAG
+CLOCKTICKFLAG = False
+global IMAGECHANGEFLAG
+IMAGECHANGEFLAG = False
+global EXITFLAG
+EXITFLAG = False
+global CLOCKTHREAD,IMAGETHREAD
 global FONT
 global CLOCK
 global Role1
@@ -35,12 +44,13 @@ class MaGame():
         #pygame.display.update()
 
 #初始化pygame游戏主窗口，可以设置窗口大小和SCREEN背景色，默认白色
-mygame = MaGame(600,600,MaColor.black)
+mygame = MaGame(600,600)
 
 def initialize():
     #变量初始化放这里
     global FONT,CLOCK,DIR
     global SCREEN_WIDTH,SCREEN_HEIGHT
+    global CLOCKTHREAD,EXITFLAG
     #在这里修改GamePrint的字体大小
     fontsize = 40
     SCREEN_WIDTH = mygame.Width
@@ -48,7 +58,19 @@ def initialize():
     DIR = os.path.split(os.path.abspath(__file__))[0]
     FONT = pygame.font.Font(None,fontsize)
     CLOCK = pygame.time.Clock()#设置帧率
+    CLOCKTHREAD = threading.Thread(target = ClockTick,args = (0.02,),name = 'tick')
+    IMAGETHREAD = threading.Thread(target = ImageTick,args = (0.5,),name = 'change image')
+    CLOCKTHREAD.start()
+    IMAGETHREAD.start()
+    #join会阻塞主线程
+    #CLOCKTHREAD.join()
+    
     #end of initialize
+
+def exitall():
+    global EXITFLAG
+    EXITFLAG = True
+    return
 
 def ClearScreen():
     #清屏
@@ -57,22 +79,53 @@ def ClearScreen():
     #pygame.display.update()
     #end of ClearSCreen
 
+def ClockTick(delay):
+    global EXITFLAG
+    global CLOCKTICKFLAG
+    while(not EXITFLAG):
+        time.sleep(delay)
+        CLOCKTICKFLAG = True
+
+#切换显示图像的速率和移动的速率不一致
+def ImageTick(delay):
+    global EXITFLAG
+    global IMAGECHANGEFLAG
+    while(not EXITFLAG):
+        time.sleep(delay)
+        IMAGECHANGEFLAG = True
+
 def FrameTask():
     #每帧的任务
     global CLOCK,SCREEN
-    global Player
+    global Player,Enemy_Orange,Enemy_Blue,Enemy_Red
     global GroupAll
+    global IMAGECHANGEFLAG
     #Frame = 40 fps   
-    CLOCK.tick(5)    
+    #CLOCK.tick(60) 
+    #用tick的方法延时会带来卡顿，我决定开一个新的线程来做定时   
     #when you need to clear the screen
     ClearScreen() 
     Player.MoveByXY(Player.Xspeed,Player.Yspeed)
-    if Player.ImgIndex == 0:
-        Player.image = Player.images[1]
-        Player.ImgIndex = 1
-    else:
-        Player.image = Player.images[0]
-        Player.ImgIndex = 0
+    Enemy_Pink.MoveByXY(Player.Xspeed,Player.Yspeed)
+    Enemy_Blue.MoveByXY(Player.Xspeed,Player.Yspeed)
+    Enemy_Orange.MoveByXY(Player.Xspeed,Player.Yspeed)
+    Enemy_Red.MoveByXY(Player.Xspeed,Player.Yspeed)
+    if Player.rect.left >= 600:
+        Player.rect.left = 0
+    if Player.rect.left < 0:
+        Player.rect.left = 600
+    if Player.rect.top >= 600:
+        Player.rect.top = 0
+    if Player.rect.top < 0:
+        Player.rect.top = 600
+    if(IMAGECHANGEFLAG):
+        IMAGECHANGEFLAG = False
+        if Player.ImgIndex == 0:
+            Player.image = Player.images[1]
+            Player.ImgIndex = 1
+        else:
+            Player.image = Player.images[0]
+            Player.ImgIndex = 0
     GroupAll.draw(SCREEN)  
     #end of FrameTask
 
@@ -92,9 +145,16 @@ class Actor(pygame.sprite.Sprite):
         self.Width,self.Height = self.image.get_size()
         self.ImgIndex = 0
         #延X、Y方向移动的像素
-        self.Speed = 25
+        self.Speed = 5
         self.Xspeed = self.Speed
-        self.Yspeed = 0        
+        self.Yspeed = 0  
+        #角色的角度
+        self.Angle = 0 
+        self.SetPosition(position[0],position[1])
+        print(self.rect)
+    def SetPosition(self,_X,_Y):
+        self.rect.top = _Y
+        self.rect.left = _X     
     def AppendImage(self,imgpath):
         global DIR
         imgpath = os.path.join(DIR, imgpath)
@@ -102,6 +162,16 @@ class Actor(pygame.sprite.Sprite):
         self.images.append(surf)
     def MoveByXY(self,dx,dy):
         self.rect.move_ip(dx,dy)
+    def Rotate(self,_angle):
+        self.Angle = _angle
+        tmpsurf = pygame.transform.rotate(self.image,_angle)
+        self.image = tmpsurf
+        for i in range(len(self.images)):
+            tmpsurf = pygame.transform.rotate(self.images[i],_angle)
+            self.images[i] = tmpsurf
+    def SetAngle(self,_angle):
+        self.Rotate(-self.Angle)
+        self.Rotate(_angle)
     #Sprite功能很强大，可以不用去定义Show函数，采用下面的方法实现显示会更便捷：
     #先定义一个Group，<group name> = pygame.sprite.Group()
     #然后将先前声明的Sprite实例加入到这个Group中，<group name>.add(<actor name>)
@@ -121,8 +191,17 @@ class Actor(pygame.sprite.Sprite):
             self.images[i] = pygame.transform.smoothscale(self.images[i],(_width,_height))
         self.image = self.images[0]
 
+class Enemy(Actor):
+    def __init__(self,_pos = (0,0)):
+        Actor.__init__(self,"Role.png",_pos)
+    def SetImage(self,_index):
+        #子类直接修改父类的属性，用self访问
+        self.rect = pygame.Rect(50*(_index+1),0,50,50)
+        self.image = self.image.subsurface(self.rect)
+        self.images[0] = self.image
+
 global AllImg #所有的角色的图像都在一副png图片里
-global Player
+global Player,Enemy_Pink,Enemy_Orange,Enemy_Blue,Enemy_Red
 global GroupAll
 global MapMatrix
 
@@ -130,34 +209,54 @@ MapMatrix = numpy.zeros((24,24),numpy.int)
 
 def main():
     global SCREEN
-    global Player
+    global Player,Enemy_Pink,Enemy_Orange,Enemy_Blue,Enemy_Red
     global AllImg
     global GroupAll
+    global CLOCKTICKFLAG
     initialize() 
 
     AllImg = Actor("Role.png")
     Player = Actor("Role.png")
+    '''
+    修改这里！！！！！！！！！！！！！！！！！！！！！！！！！
+    '''
+    #在SetImage中重新定义了Rect，所以目前来说初始化的_pos没用了，这部分需要修改
+    Enemy_Pink = Enemy(_pos = (100,100))
+    Enemy_Pink.SetImage(1)
+    Enemy_Blue = Enemy(_pos = (200,100))
+    Enemy_Blue.SetImage(2)
+    Enemy_Orange = Enemy(_pos = (300,100))
+    Enemy_Orange.SetImage(3)
+    Enemy_Red = Enemy(_pos = (400,100))
+    Enemy_Red.SetImage(4)
+    
     Player.rect = pygame.Rect(0,0,50,50)
     Player.image = AllImg.image.subsurface(Player.rect)
     Player.images[0] = Player.image
     tmprect = pygame.Rect(50,0,50,50)
     Player.images.append(AllImg.image.subsurface(tmprect))
-
+    
     GroupAll = pygame.sprite.Group() 
     GroupAll.add(Player) 
+    GroupAll.add(Enemy_Pink,Enemy_Blue,Enemy_Orange,Enemy_Red)
     GroupAll.draw(SCREEN) 
+
     while True:          
-        pygame.display.update()      
-        FrameTask()
+        pygame.display.update()  
+        if(CLOCKTICKFLAG):  
+            CLOCKTICKFLAG = False  
+            FrameTask()            
         for event in pygame.event.get():
             #事件捕捉          
             if event.type == QUIT:
+                exitall()
                 return
             if event.type == MOUSEMOTION:
                 #鼠标移动事件
                 pass
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
+                    exitall()
                     return
                 #按键处理程序
                 if event.key == 13:
@@ -166,15 +265,19 @@ def main():
                 if event.key == K_LEFT:
                     Player.Xspeed = -Player.Speed
                     Player.Yspeed = 0
+                    Player.SetAngle(-180)
                 if event.key == K_RIGHT:
                     Player.Xspeed = Player.Speed
                     Player.Yspeed = 0
+                    Player.SetAngle(0)
                 if event.key == K_UP:
                     Player.Xspeed = 0
                     Player.Yspeed = -Player.Speed
+                    Player.SetAngle(90)
                 if event.key == K_DOWN:
                     Player.Xspeed = 0
                     Player.Yspeed = Player.Speed
+                    Player.SetAngle(-90)
 
     #end of main
 
